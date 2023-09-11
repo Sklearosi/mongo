4445197a49a036bf0a3f474642d9833054c65430
Sklearosi <giorgi.chanturidze.2000@gmail.com>
2023-09-12 01:52:48 +0400

    fix: added in router

--- src/routes/job-router.ts
+++ src/routes/job-router.ts
@@ -1,10 +1,11 @@
-import { createJob, getJobs } from 'controller/jobs-controller';
+import { createJob, getAllJobs, getJobs } from 'controller/jobs-controller';
 import express from 'express'
 
 const jobRouter = express.Router();
 
 jobRouter.post("/job-insert", createJob)
 jobRouter.get("/job-get", getJobs )
+jobRouter.get("/all-job", getAllJobs)
 
 
 export default jobRouter
